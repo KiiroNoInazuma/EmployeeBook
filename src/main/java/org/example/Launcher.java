8de31c9e1b5749581c70e87d7cc4a8f8b9998bec
John Rey <adioskid@mail.ru>
2024-06-23 13:08:49 +0400

    #1

--- src/main/java/org/example/Launcher.java
+++ src/main/java/org/example/Launcher.java
@@ -20,7 +20,7 @@ public class Launcher {
         Employee emp5 = new Employee("Матросов Григорий Иванович", "2", 570_000);
         Employee emp6 = new Employee("Мяэ Михаил Александрович", "4", 370_000);
         Employee emp7 = new Employee("Костылев Александр Александрович", "4", 370_000);
-        Employee emp8 = new Employee("Прокопьева Софья Леонидовна", "3", 160_000);
+        Employee emp8 = new Employee("Прокопьева Софья Леонидовна", "3", 130_000);
 
         employeeBook.addEmployee(emp1);
         employeeBook.addEmployee(emp2);
